e781567af7768dd6acad333f58f37f4462c3678f
ZEC <1483713539@qq.com>
2021-09-02 20:09:50 +0800

    test second commit

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -7,6 +7,6 @@ public class test {
         System.out.println(1);
         System.out.println(2);
         System.out.println(3);
-        System.out.println(6);
+        System.out.println(7);
     }
 }
